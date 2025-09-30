e4b86e0675bc685a70e1eef25b2b82a281f96f78
Pauldarlefelbacq <paul.darle-felbacq@edu.univ-fcomte.fr>
2025-09-30 11:30:56 +0200

    fin tp (traduction de toutes les pages)

---
+++ src/i18n/ui.js
@@ -0,0 +1,74 @@
+export const ui = {
+en: {
+    nav: {
+      home: 'Home',
+      generator: 'Generator',
+      gallery: 'Gallery',
+      language: 'Language',
+    },
+    index: {
+        title: 'Welcome to SVG Generator',
+        description: 'Create and render SVGs from prompts.',
+        button: 'Go to SVG Generator',
+    },
+    generator: {
+        title: 'SVG Generator',
+        contentPlaceholder: 'Declare the svg you want to generate',
+        codePlaceholder: 'SVG code will be displayed here',
+        promptLabel: 'Enter your prompt:',
+        generateButton: 'Generate',
+        editButton: 'Edit',
+        viewButton: 'View',
+        previewPlaceholder: 'SVG preview will appear here...',
+        saveButton: 'Save',
+    },
+    gallery: {
+        title: 'SVG Gallery',
+        viewDetails: 'View Details',
+        generatebutton:"Generate",
+
+        
+    },
+    id:{
+        chaterror:'No chat history',
+        chathistory: 'Chat history',
+        enterprompt: 'Enter a prompt to edit the SVG',
+        edit: 'Edit',
+    }
+  },
+  fr: {
+    nav: {
+      home: 'Accueil',
+      generator: 'Générateur',
+      gallery: 'Galerie',
+      language: 'Langue',
+    },
+    index: {
+        title: 'Bienvenue sur le générateur SVG',
+        description: 'Créez et affichez des SVG à partir d\'invites.',
+        button: 'Aller au générateur SVG',
+    },
+    generator: {
+        title: 'Générateur SVG',
+        contentPlaceholder: 'Décrivez le svg que vous voulez générer',
+        codePlaceholder: 'Le code SVG sera affiché ici',
+        promptLabel: 'Entrez votre invite :',
+        generateButton: 'Générer',
+        editButton: 'Éditer',
+        viewButton: 'Voir',
+        previewPlaceholder: 'L\'aperçu SVG apparaîtra ici...',
+        saveButton: 'Enregistrer',
+    },
+    gallery: {
+        title: 'Galerie SVG',
+        viewDetails: 'Voir les détails',
+        generatebutton:"Générer",
+    },
+    id:{
+        chaterror:'Aucun historique de chat.',
+        chathistory: 'Historique de chat',
+        enterprompt: 'Entrer un prompt pour éditer le svg',
+        edit: 'Editer',
+    }
+  },
+}
